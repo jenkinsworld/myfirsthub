49fa4fd8a3727f9921ca847f9d50226e334ad46d
chandrasekhar <gchandrasekhar.cm@gmail.com>
2018-12-20 16:12:04 +0530

    script to increment the version

--- script.sh
+++ script.sh
@@ -1,1 +1,7 @@
-git describe --abbrev=0 --tags\par
+!/bin/bash
+tag=git describe --abbrev=0 --tags| cut -d '.' -f 3
+min_ver= tag | cut -d '.' -f 3
+inc_ver=val+1
+tag|sed -i 's/inc_ver/min_ver/g'
+
+
