6ae6cd439d10360ec942208c85c1be60283da347
chandrasekhar <gchandrasekhar.cm@gmail.com>
2018-12-20 13:46:53 +0530

    Rename script.sh.rtf to script.sh

---
+++ script.sh
@@ -0,0 +1,6 @@
+{\rtf1\ansi\ansicpg1252\deff0\nouicompat\deflang1033{\fonttbl{\f0\fnil\fcharset0 Calibri;}}
+{\*\generator Riched20 10.0.15063}\viewkind4\uc1 
+\pard\sa200\sl276\slmult1\f0\fs28\lang9 git describe --abbrev=0 --tags\par
+\par
+\par
+}
